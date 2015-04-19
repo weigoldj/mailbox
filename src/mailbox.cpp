9b088c7fb54fde1339f8dab7639358f3afa0d94c
johann <weigoldj@gmail.com>
2015-04-19 15:27:18 -0600

    Read a directory

--- src/mailbox.cpp
+++ src/mailbox.cpp
@@ -1,5 +1,8 @@
-#include "mailbox.h"
 
+#include <iostream>
+#include <dirent.h>
+
+#include "mailbox.h"
 
 Mailbox::Mailbox(void) {
   cout << "Mailbox constructor" << endl;
@@ -11,4 +14,14 @@ Mailbox::~Mailbox() {
 
 void Mailbox::monitor_dir(void) {
   cout << "starting monitor" << endl;
+  string dir_name = "./tmp";
+ 
+  DIR *dir;  
+  struct dirent *s_dir;
+  dir = opendir(dir_name.c_str());
+  //  # TODO check that dir is not null
+  
+  while((s_dir = readdir(dir))) { 
+    cout << s_dir->d_name << endl; 
+  };
 }
